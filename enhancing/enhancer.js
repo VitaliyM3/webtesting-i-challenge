990344d3dd2aa1fde2edbc734cc784f4433ad8e7
Vitaliy Mikhaylyuk <VitaliyMikhaylyuk@gmail.com>
2020-06-11 21:23:15 -0400

    almost done

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,18 +5,52 @@ module.exports = {
   get,
 };
 
+// function succeed(item) {
+//   return { ...item };
+// }
+
 function succeed(item) {
-  return { ...item };
+  const enhance = item.enhancement;
+
+  if(enhance == 20) {
+    return (
+      "its already maxxed"
+    )
+  } else {
+    return { ...item, enhancement: enhance + 1 };
+  }
 }
+
+// function fail(item) {
+//   return { ...item };
+// }
 
 function fail(item) {
-  return { ...item };
+  const {enhancement, durability} = item;
+
+  if (enhancement < 15) {
+    return {...item, durability: durability - 5};
+  } else if (enhancement > 14 && enhancement < 17) {
+    return {...item, durability: durability - 10};
+  } else if (enhancement > 16) {
+    return {...item, durability: durability - 10, enhancement: enhancement -1};
+  };
 }
 
+// function repair(item) {
+//   return { ...item };
+// }
+
 function repair(item) {
+
+  if(item) {
+    item.durability = 100;
+  }
   return { ...item };
 }
 
 function get(item) {
   return { ...item };
 }
+
+
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,67 @@
-const enhancer = require('./enhancer.js');
+// const enhancer = require('./enhancer.js');
+const {succeed, fail, repair, get} = require('./enhancer.js');
+
 // test away!
+
+describe('repair item', function() {
+    describe(".repair()", function() {
+        it('should take an item and restore its durability to 100%', function() {
+            const item = {
+                name: "sword",
+                durability: 90,
+                enhancement: 17
+            };
+
+            const repairedItem = {
+                name: "sword",
+                durability: 100,
+                enhancement: 17
+            };
+
+            const repairItem = repair(item);
+            expect(repairItem).toEqual(repairedItem);
+        });
+    });
+});
+
+describe('enhancment succeeds', function() {
+    describe(".succeed()", function() {
+        it('should increase enhancement by 1 with a max of 20', function() {
+            const item = {
+                name: "sword",
+                durability: 75,
+                enhancement: 17
+            };
+
+            const enhanced = {
+                name: "sword",
+                durability: 75,
+                enhancement: 18
+            };
+
+            const enhanceItem = succeed(item);
+            expect(enhanced).toEqual(enhanceItem);
+        });
+    });
+});
+
+describe('enhancement fails', function() {
+    describe(".fail if enh < 15", function() {
+        it('if enh is < 15, dec dur by 5. if enh is 15 or more, dec dur by 10. if item enh > 16, enh dec by 1', function() {
+            const item = {
+                name: "sword",
+                durability: 75,
+                enhancement: 13
+            };
+
+            const expectedRes = {
+                name: "sword",
+                durability: 70,
+                enhancement: 13
+            };
+
+            const failedItem = fail(item);
+            expect(failedItem).toEqual(expectedRes);
+        });
+    });
+});
